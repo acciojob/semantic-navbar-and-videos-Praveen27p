6fffb438f01878a527024aeaa310ccae576d4d20
Praveen27p <122203672+Praveen27p@users.noreply.github.com>
2023-10-19 19:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 //your code here
-<video width="320" height="240" controls>
-           <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" >
-           <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" >
-           <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4">
-        </video>
